0cbe35850d4175c835e0ba1c0974f145468b8cef
varxyz <mrsergiumoraru@gmail.com>
2017-04-12 14:17:16 +0300

    watchlist edit

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,6 @@
 // See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
 // about the code splitting business
 import { getAsyncInjectors } from './utils/asyncInjectors';
-import { isAuthenticated, selectGlobal } from 'containers/app/selectors';
-import { makeSelectRepos, makeSelectQueryname, makeSelectAuth } from 'containers/App/selectors';
-import { AuthState } from 'containers/auth/reducer';
 
 const errorLoading = (err) => {
   console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
